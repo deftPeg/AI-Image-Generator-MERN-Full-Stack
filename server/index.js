8711c6c6cb918d27fba8a3b25a84cb6bf9144f22
Peggy Kalliani <pegztales@gmail.com>
2023-02-27 22:35:24 +0200

    created backend

--- server/index.js
+++ server/index.js
@@ -3,14 +3,25 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+// import internal components
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+
 // function to pull variables from dotenv file
 dotenv.config();
 
 // function to initialize express application
 const app = express();
+
 // pass middlewares
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+// import routes
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 // create routes
 app.get('/', async (req, res) => {
@@ -19,9 +30,13 @@ app.get('/', async (req, res) => {
     });
   });
 
-// function to run application / start server
-const startServer = async () => {
+  const startServer = async () => {
+    try {
+      connectDB(process.env.MONGODB_URL);
       app.listen(8080, () => console.log('Server started on port 8080'));
+    } catch (error) {
+      console.log(error);
+    }
   };
   
   startServer();
